80c992bf9a983a5a93c497f4d5996a4f6f1d7c3b
Gonzalo Frank <devgonzalofrank@icloud.com>
2023-05-26 21:04:02 -0300

    creacion archivo ejercicio3

---
+++ Ejercicios/src/ejercicio3.java
@@ -0,0 +1,9 @@
+/*Escribir un programa que lea 
+dos números y realice el cálculo de la suma, 
+la resta, la multiplicación y la división entre 
+ambos valores. Los resultados deben mostrarse 
+por pantalla. */
+
+public class ejercicio3 {
+    
+}
