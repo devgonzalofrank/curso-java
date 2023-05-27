3fbf755960f2c803b14cfe264b8ba7bd137cf00f
Gonzalo Frank <devgonzalofrank@icloud.com>
2023-05-26 22:04:33 -0300

    desarrollo ejercicio 4

---
+++ Ejercicios/src/Ejercicio4.java
@@ -0,0 +1,20 @@
+/*Escribir un programa que lea la estatura de tres personas, calcule el promedio de la altura de ellos y lo informe.*/
+
+import java.util.Scanner;
+
+
+public class Ejercicio4 {
+    
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Introduce la altura de tres personas, separadas por un enter");
+        float alt1 = s.nextFloat();
+        float alt2 = s.nextFloat();
+        float alt3 = s.nextFloat();
+
+        float prom = ((alt1 + alt2 + alt3)/3);
+
+        System.out.println("El promedio de las 3 alturas es: " + prom);
+    }
+
+}
