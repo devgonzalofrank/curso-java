c862d0c5f7a376e7470eb17974f80d7f88f52bd9
Gonzalo Frank <devgonzalofrank@icloud.com>
2023-06-01 20:19:45 -0300

    Resolucion ejercicio 5 guia 1

--- Ejercicios/src/Ejercicio6.java
+++ Ejercicios/src/Ejercicio6.java
@@ -1,3 +1,24 @@
+/*Pedir al usuario que ingrese el 
+precio de un producto y el porcentaje de descuento. A continuación mostrar por pantalla el 
+importe descontado y el importe a pagar. */
+
+import java.util.Scanner;
+
 public class Ejercicio6 {
     
+    public static void main(String[] args){
+            Scanner s = new Scanner(System.in);
+            System.out.println("Introduzca el precio del producto");
+            float precio = s.nextFloat();
+            System.out.println("Ingrese el porcentaje de descuento, solo el numero");
+            float porc = s.nextFloat();
+
+            float descuento = precio/100*porc;
+            float aPagar = precio-descuento;
+
+            System.out.println("El importe a pagar con descuento es: " + aPagar + ", el descuento es de: " + descuento);
+
+            s.close();
+    }
+
 }
